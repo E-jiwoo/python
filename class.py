c7c1c7d28620448f2e5b21495f3073bdd223a575
E-jiwoo <livei07@naver.com>
2023-10-12 15:47:20 +0900

    FishCakeMaker

--- class.py
+++ class.py
@@ -60,6 +60,33 @@ class Grade:
     def __str__(self):
         return "%s : %c 등급" %(self.name, self.score)
     
-a1=Grade("나영", 89)
+a1=Grade("지우", 100)
 a1.grade()
 print(a1)
+
+class FishCakeMaker:
+    def __init__(self, **kwargs): #** : 가변인자 매개변수
+        self.size=10
+        self.flavor="팥"
+        self.price=100
+
+        if "size" in kwargs:
+            self.size=kwargs.get("size") #kwargs 딕셔너리 안에 size라는 key값이 있니? 있다면 가져와서 size 변수에 그 해당하는 value값을 다시 대입하겠다
+        if "flavor" in kwargs:
+            self.flavor=kwargs.get("flavor")
+        if "price" in kwargs:
+            self.price=kwargs.get("price")
+    
+    def show(self):
+        print("붕어빵 크기{}".format(self.size))
+        print("붕어빵 종류{}".format(self.flavor))
+        print("붕어빵 가격{}".format(self.price))
+        print("*"*30)
+
+fish1=FishCakeMaker()
+fish2=FishCakeMaker(size=20, price=300)
+fish3=FishCakeMaker(flavor="초코", size=15)
+fish1.show()
+fish2.show()
+fish3.show()
+        
