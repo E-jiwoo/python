70b08ff13bc2ae08b4042133c931f6665c29fdaf
E-jiwoo <livei07@naver.com>
2023-10-12 15:51:56 +0900

    MarketGoods

--- class.py
+++ class.py
@@ -90,3 +90,14 @@ fish1.show()
 fish2.show()
 fish3.show()
         
+#붕어빵기계 클래스를 상속받은 마켓굿즈
+    
+class MarketGoods(FishCakeMaker):
+    def __init__(self, margin=1000, **kwargs):
+        super().__init__(**kwargs)
+        self.market_price = self.price + margin
+    def show(self):
+        print(self.flavor, self.market_price)
+
+fish1=MarketGoods(size=20, price=500)
+fish1.show()
